d6d5ad4fc87fe4a3934d75a33eb0b5dd753ad6b9
annastacia <annastaciangina96@gmail.com>
2022-09-11 01:21:42 +0300

    adding background image

---
+++ src/components/Slider.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Slider() {
+    return (
+        <nav id="slider">
+             <div className="slider-image">
+              <p>lorem ipsum </p>
+            </div>
+
+        </nav>
+    );
+}
+export default Slider;
