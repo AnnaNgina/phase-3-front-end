2995e0586c239ec4b7e5fcf0f4e577059dab6de6
annastacia <annastaciangina96@gmail.com>
2022-09-11 23:14:15 +0300

    commit

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,27 @@
-import React from 'react';
-function Posts() {
-    return (
-        <div id="post-container">
-             <div className="post">
-              <p>Add post</p>
-            </div>
+import React, {useEffect, useState }from 'react';
 
-        </div>
-    );
-}
+
+function Posts() {
+
+    const [posts, setPosts] = useState ([])
+  useEffect(() => {
+    fetch("http://localhost:9292/recipes")
+    .then((response) => response.json())
+    .then((data) =>{
+      setPosts(data)
+      console.log(data)
+    })
+  },[]);
+   return (
+    <div className='post-container'>
+              { posts.map((post) => (
+                <ol id={post.id} key={post.id} >
+                <p>Name: {post.name}</p>
+                <p>Author: {post.author}</p>
+                <p>Ingredients: {post.ingredients}</p>
+                </ol>
+            ))}
+    </div>
+);
+              }
 export default Posts;
