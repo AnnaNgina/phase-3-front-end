029fe246e6f0627cafe594fcbdf1ef58403da995
annastacia <annastaciangina96@gmail.com>
2022-09-11 11:08:00 +0300

    add

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,7 +3,8 @@ function Slider() {
     return (
         <nav id="slider">
              <div className="slider-image">
-              <p>The system provide a platform for food,drinks and fruits review. It provides access to posting recipe for any meal.</p>
+              <p>The system provide a platform for food,drinks </p>
+              <p>and fruits review. It provides access to posting recipe for any meal.</p>
             </div>
 
         </nav>
